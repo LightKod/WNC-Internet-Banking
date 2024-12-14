c640d47e580ba7ddffa18cce969a28c7579f978a
PICKLE\hoang <21127092@student.hcmus.edu.vn>
2024-12-14 11:01:13 +0700

    added create payment request page layout

--- front-end/app/lib/schemas/schemas.ts
+++ front-end/app/lib/schemas/schemas.ts
@@ -55,4 +55,39 @@ export const cancelPaymentRequestSchema = z.object({
     })
 })
 
-export type CanclePaymentRequestFormValue = z.infer<typeof cancelPaymentRequestSchema>
+export type CancelPaymentRequestFormValue = z.infer<typeof cancelPaymentRequestSchema>
+
+export const paymentRequestSchema = z.object({
+    senderAccountNumber: z.string({
+        invalid_type_error: "Invalid data",
+        required_error: "This field is required"
+    }).length(15, {
+        message: "Invalid account number"
+    }),
+    receiverAccountNumber: z.string({
+        invalid_type_error: "Invalid data",
+        required_error: "This field is required"
+    }).trim().length(12, {
+        message: "You must specify a receiver"
+    }),
+    amount: z.coerce.number({
+        invalid_type_error: "Invalid data",
+        required_error: "This field is required"
+    }).int({
+        message: "Amount must be an integer"
+    }).positive({
+        message: "Amount cannot be negative"
+    }).min(10000, {
+        message: "The minimum amount for a request is 10,000 VND"
+    }),
+    requestNote: z.string({
+        invalid_type_error: "Invalid data",
+        required_error: "This field is required"
+    }).min(1, {
+        message: "This field is required"
+    }).max(500, {
+        message: "The content is too long (max: 500 characters)"
+    })
+})
+
+export type PaymentRequestFormValue = z.infer<typeof paymentRequestSchema>
---
+++ front-end/app/ui/components/payment_request/successful_request.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { CheckCircleIcon } from "@heroicons/react/24/outline"
+import { useContext, useState } from "react"
+import { PageContentContext } from "./create_payment_request_content"
+import { formatAccountNumber, formatMoney } from "@/app/lib/utilities/utilities"
+import { ArrowLeftIcon, ArrowPathIcon, ArrowRightIcon } from "@heroicons/react/16/solid"
+import Link from "next/link"
+
+export default function SuccessfulRequest() {
+    const context = useContext(PageContentContext)
+
+    if(!context){
+        throw new Error('Something went wrong')
+    }
+
+    const formValues = context.formRef.current?.getValues()
+    const receiverBankAccount = context.formRef.current?.receiverBankAccount
+
+    return (
+        <div className="flex flex-col gap-y-8 px-8 pb-8">
+            <div className="flex flex-col gap-y-1 items-center p-8 bg-blue-50 rounded-md border-2 border-blue-100">
+                <CheckCircleIcon className="w-24 text-blue-600"/>
+                <p className="text-gray-950 font-semibold">Successful request</p>
+                <p className="text-sm text-gray-500">Your payment request has been successfully created</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="flex flex-col gap-y-4">
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Receive source</div>
+                        <div className="text-gray-500">{formatAccountNumber(formValues!.senderAccountNumber)}</div>
+                    </div>
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Debtor's account number</div>
+                        <div className="text-gray-500">{formatAccountNumber(formValues!.receiverAccountNumber)}</div>
+                    </div>
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Debtor's name</div>
+                        <div className="text-blue-600">{receiverBankAccount?.name}</div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-4">
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Transfer type</div>
+                        <div className="text-gray-500">Debt payment</div>
+                    </div>
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Debt amount</div>
+                        <div className="text-blue-600">{`${formatMoney(formValues!.amount.toString())} VND`}</div>
+                    </div>
+                    <div className="flex flex-col gap-y-0.5">
+                        <div className="text-sm text-gray-950 font-semibold">Note</div>
+                        <div className="text-gray-500">{formValues?.requestNote}</div>
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <Link href="/dashboard" className="flex items-center justify-center gap-2 rounded-md px-3 py-2.5 bg-slate-200 text-gray-950 text-sm font-medium hover:bg-slate-300 transition-colors duration-300">
+                    <ArrowLeftIcon className="w-4"/>
+                    <p>Dashboard</p>
+                </Link>
+                <button type="button" onClick={() => window.location.reload()} className="flex items-center justify-center gap-2 rounded-md px-3 py-2.5 bg-blue-600 text-blue-50 text-sm font-medium hover:bg-blue-700 transition-colors duration-300">
+                    <ArrowPathIcon className="w-4"/>
+                    <p>Make a new request</p>
+                </button>
+                <Link href="/payment-request" className="flex items-center justify-center gap-2 rounded-md px-3 py-2.5 bg-slate-200 text-gray-950 text-sm font-medium hover:bg-slate-300 transition-colors duration-300">
+                    <ArrowRightIcon className="w-4"/>
+                    <p>Payment request</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
