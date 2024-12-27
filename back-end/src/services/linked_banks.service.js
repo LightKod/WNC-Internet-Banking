3d0513a4d689a62a4281913acdf4b0c3a868d0a2
Quang <nguyennhatquangkod2003@gmail.com>
2024-12-27 23:34:30 +0700

    feat: create linked bank
    set to admin only later

--- back-end/src/services/linked_banks.service.js
+++ back-end/src/services/linked_banks.service.js
@@ -6,11 +6,33 @@ const { LinkedBanks } = db;
 export const getAllLinkedBanksService = async () => {
     try {
         const linkedBanks = await LinkedBanks.findAll({
-            attributes: ['id', 'bank_code', 'bank_name', 'encryption_type', 'api_base_url'],
+            attributes: ['id', 'bank_code', 'bank_name'],
         });
         return linkedBanks;
     } catch (error) {
         console.error("Error fetching linked banks:", error);
         throw new Error("Failed to fetch linked banks");
     }
+};
+
+
+export const createLinkedBankService = async (linkedBankData) => {
+    const { bank_code, bank_name, public_key, secret_key, encryption_type, account_info_api_url, deposit_api_url } = linkedBankData;
+
+    const existingBank = await LinkedBanks.findOne({ where: { bank_code } });
+    if (existingBank) {
+        throw new Error("A bank with this bank_code already exists");
+    }
+
+    const newLinkedBank = await LinkedBanks.create({
+        bank_code,
+        bank_name,
+        public_key,
+        secret_key,
+        encryption_type,
+        account_info_api_url,
+        deposit_api_url,
+    });
+
+    return newLinkedBank;
 };
