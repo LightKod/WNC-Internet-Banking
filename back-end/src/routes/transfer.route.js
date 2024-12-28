5e222345a70ed30769264efd884cf0877b164dc8
Tran-Minh-Khoa <99416417+Tran-Minh-Khoa@users.noreply.github.com>
2024-12-28 10:50:06 +0700

    add container service pgp

--- back-end/src/routes/transfer.route.js
+++ back-end/src/routes/transfer.route.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import transferController from '../controllers/transfer.controller.js';
-
+import passport from "passport";
 
 const router = express.Router();
+const protectRoute = passport.authenticate('jwt', { session: false });
 
 //Internal
-router.post('/internal/initiate', transferController.initiateTransfer);
-router.post('/internal/confirm', transferController.confirmTransfer);
-router.post('/internal/deposit', transferController.depositInternal);
+router.post('/internal/initiate',protectRoute, transferController.initiateTransfer);
+router.post('/internal/confirm',protectRoute, transferController.confirmTransfer);
+router.post('/internal/deposit',protectRoute, transferController.depositInternal);
 
 //External
-router.post('/external/initiate', transferController.initiateExternalTransfer);
-router.post('/external/confirm', transferController.confirmExternalTransfer);
-router.post('/external/account-info', transferController.accountInfo);
+router.post('/external/initiate',protectRoute, transferController.initiateExternalTransfer);
+router.post('/external/confirm', protectRoute,transferController.confirmExternalTransfer);
+router.post('/external/account-info',transferController.accountInfo);
 router.post('/external/deposit', transferController.deposit);
-router.post('/external/link', transferController.linkBank);
+router.post('/external/link',protectRoute, transferController.linkBank);
 
 export default router;
